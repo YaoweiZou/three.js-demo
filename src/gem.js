1ed45d29caab09d5836898c9889d240e800eb01f
Yaowei <yaoweipublic@icloud.com>
2023-08-20 22:59:13 +0800

    refactor gem.js;

--- src/gem.js
+++ src/gem.js
@@ -14,22 +14,32 @@ import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import Stats from "three/examples/jsm/libs/stats.module.js";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 
-function main() {
-  render();
-}
-
-const size = {
-  width: innerWidth,
-  height: innerHeight
-}
-
 const scene = new Scene();
+
+const camera = new PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 100);
+camera.position.set(2, 2, 6);
+scene.add(camera);
+
+const renderer = new WebGLRenderer({
+  precision: "highp",
+  antialias: true,
+  alpha: true
+});
+renderer.setClearColor("#000000", 0);
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = PCFSoftShadowMap;
+document.body.appendChild(renderer.domElement);
+
+window.onresize = () => {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+}
 
 const loader = new GLTFLoader();
 loader.load("/gem/gem.gltf", gltf => {
-  // gltf.scene.children.forEach(item => {
-  //   console.log(item);
-  // })
   const gem = gltf.scene.children[0];
   gem.material.roughnessMap = new TextureLoader().load("/gem/roughness.jpeg");
   gem.material.displacementScale = 0.15;
@@ -48,22 +58,6 @@ scene.add(light1);
 const light2 = new DirectionalLight(0xffffff, 3);
 light2.position.set(-1, -1, -1);
 scene.add(light2);
-
-const camera = new PerspectiveCamera(75, size.width / size.height, 0.1, 100);
-camera.position.set(2, 2, 6);
-scene.add(camera);
-
-const renderer = new WebGLRenderer({
-  precision: "highp",
-  antialias: true,
-  alpha: true
-});
-renderer.setClearColor("#000000", 0);
-renderer.setSize(size.width, size.height);
-renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-renderer.shadowMap.enabled = true;
-renderer.shadowMap.type = PCFSoftShadowMap;
-document.body.appendChild(renderer.domElement);
 
 let controls = new OrbitControls(camera, renderer.domElement);
 controls.target.set(0, 0.75, 0);
@@ -84,11 +78,11 @@ controls.enabled = true;
 // 是否启用水平或垂直旋转
 controls.enableRotate = true;
 
-window.onresize = () => {
-  camera.aspect = window.innerWidth / window.innerHeight;
-  camera.updateProjectionMatrix();
-  renderer.setSize(window.innerWidth, window.innerHeight);
-}
+function helper() {
+  const axesHelper = new AxesHelper(100);
+  scene.add(axesHelper);
+};
+helper();
 
 let stats = null;
 
@@ -129,15 +123,9 @@ function render() {
   requestAnimationFrame(render);
 };
 
-function helper() {
-  const axesHelper = new AxesHelper(100);
-  scene.add(axesHelper);
-};
-helper();
-
 // WebGL 是否可用
 if (WebGL.isWebGLAvailable()) {
-  main();
+  render();
 } else {
   const errorMessage = WebGL.getWebGLErrorMessage();
   document.body.appendChild(errorMessage);
